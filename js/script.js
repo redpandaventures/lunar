8786e346f433a9b9db17b09d250a1c065abcd362
richard swain <richard@humanmade.co.uk>
2016-09-02 10:45:36 +0100

    CS

--- js/script.js
+++ js/script.js
@@ -81,16 +81,14 @@ jQuery(window).resize(function(){
     // Offset the anchor tag link on weekly update comments to allow for nav and admin bars.
     $( '.avatar-link' ).click(function(){
 
-         var commentHref             = 'li' + $( this ).attr( 'href' );
-         var commentOffset           = $( commentHref ).offset();
-         var scrollto                = commentOffset.top - 72;
+        var commentHref             = 'li' + $( this ).attr( 'href' ),
+            commentOffset           = $( commentHref ).offset(),
+            scrollto                = commentOffset.top - 72,
+            originalBackgoundColour = $( commentHref ).css( 'background-color' );
 
-         var originalBackgoundColour = $( commentHref ).css( "background-color" );
-
-        $('html, body').animate({
+        $( 'html, body' ).animate({
             scrollTop:scrollto
             }, 0,
-
             // Highlight the comment, then fade away the highlight.
             function(){
                 $( commentHref ).css( { backgroundColor: '#F6F3D1' } ).animate( { backgroundColor: originalBackgoundColour }, 2000 );
